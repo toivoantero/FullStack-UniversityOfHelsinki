a0b55ec86a53e856d219ccb98844bba4f8d0f523
toivoantero <125696622+toivoantero@users.noreply.github.com>
2026-04-13 23:51:22 +0300

    tehtävät 3.1-3.9 tehty valmiiksi

---
+++ osa3/puhelinluettelo-backend/index.js
@@ -0,0 +1,91 @@
+const express = require('express')
+const cors = require('cors')
+var morgan = require('morgan')
+const app = express()
+app.use(cors())
+app.use(express.json())
+//app.use(morgan('tiny'))
+morgan.token('postissa', (req) => { return JSON.stringify(req.body) })
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :postissa'))
+
+let persons = [
+    {
+        "name": "Arto Hellas",
+        "number": "040-123456",
+        "id": "1"
+    },
+    {
+        "name": "Ada Lovelace",
+        "number": "39-44-5323523",
+        "id": "2"
+    },
+    {
+        "name": "Dan Abramov",
+        "number": "12-43-234345",
+        "id": "3"
+    },
+    {
+        "name": "Mary Poppendieck",
+        "number": "39-23-6423122",
+        "id": "4"
+    }
+]
+
+app.get('/api/persons', (request, response) => {
+    response.json(persons)
+})
+
+app.get('/info', (request, response) => {
+    response.send('<p>Phonebook has info for ' +
+        persons.length + ' people</p><p>' +
+        new Date() + '</p>')
+})
+
+app.get('/api/persons/:id', (request, response) => {
+    const id = request.params.id
+    const person = persons.find(p => p.id === id)
+
+    if (person) {
+        response.json(person)
+    } else {
+        response.status(404).end()
+    }
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+    const id = request.params.id
+    persons = persons.filter(p => p.id !== id)
+
+    response.status(204).end()
+})
+
+app.post('/api/persons', (request, response) => {
+    const body = request.body
+
+    if (!body.name || !body.number) {
+        return response.status(400).json({
+            error: 'name or number missing'
+        })
+    }
+
+    if (persons.find(p => p.name === body.name)) {
+        return response.status(400).json({
+            error: 'name must be unique'
+        })
+    }
+
+    const person = {
+        name: body.name,
+        number: body.number,
+        id: Math.floor(Math.random() * 1000).toString(),
+    }
+
+    persons = persons.concat(person)
+
+    response.json(person)
+})
+
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
